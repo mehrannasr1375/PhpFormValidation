c53ae4219eb9bec3958d6026c1ac942362327fff
Mehran Nasr <mrparadox1397@gmail.com>
2019-07-13 19:59:47 +0430

    fix typo

--- FormValidation.php
+++ FormValidation.php
@@ -94,11 +94,11 @@
     }
 
 
-	function test_input($data) {
-	  $data = trim($data);
-	  $data = stripslashes($data);
-	  $data = htmlspecialchars($data);
-	  return $data;
+    function test_input($data) {
+	$data = trim($data);
+	$data = stripslashes($data);
+	$data = htmlspecialchars($data);
+	return $data;
     }
 
 
