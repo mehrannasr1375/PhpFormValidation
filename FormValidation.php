dc436b930f8d47781255ab2359aa74ed5441ddfd
mehrannasr1375 <mrparadox1397@gmail.com>
2019-07-15 20:34:59 +0430

    add captcha

--- FormValidation.php
+++ FormValidation.php
@@ -95,11 +95,11 @@
     }
 
 
-    function test_input($data) {
-	$data = trim($data);
-	$data = stripslashes($data);
-	$data = htmlspecialchars($data);
-	return $data;
+	function test_input($data) {
+	  $data = trim($data);
+	  $data = stripslashes($data);
+	  $data = htmlspecialchars($data);
+	  return $data;
     }
 
 
